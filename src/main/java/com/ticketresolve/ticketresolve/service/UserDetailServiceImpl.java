ade94b3a3fa7ad94b926995a259bd2d947936d06
Estiven033 <egarciar@estudiante.uniajc.edu.co>
2025-09-01 00:46:28 -0500

    Agrego servicios del proyecto

---
+++ src/main/java/com/ticketresolve/ticketresolve/service/UserDetailServiceImpl.java
@@ -0,0 +1,42 @@
+package com.ticketresolve.ticketresolve.service;
+
+import java.util.Collection;
+import java.util.stream.Collectors;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.proyecto.accesoSeguroService.model.Usuario;
+import com.proyecto.accesoSeguroService.repository.UsuarioRepository;
+
+@Service
+public class UserDetailServiceImpl {
+    
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+    
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException{
+
+        Usuario usuario = usuarioRepository.findByUsername(username)
+                                        .orElseThrow(() -> new UsernameNotFoundException("El Usuario " + username + " no existe"));
+
+        Collection<? extends GrantedAuthority> authorities = usuario.getRoles()
+                .stream()
+                .map(role -> new SimpleGrantedAuthority("ROLE_".concat(role.getName().name())))
+                .collect(Collectors.toSet());
+
+        return new User(usuario.getUsername(),
+                usuario.getPassword(),
+                true,
+                true,
+                true,
+                true,
+                authorities);
+    }
+}
