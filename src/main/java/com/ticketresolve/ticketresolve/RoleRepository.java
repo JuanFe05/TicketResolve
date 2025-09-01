ad865eda57058fea7b422713609eb8d937258228
USUARIO <jeanpoolvalenciasanchez@gmail.com>
2025-08-31 22:43:23 -0500

    se crean los repositorios

---
+++ src/main/java/com/ticketresolve/ticketresolve/RoleRepository.java
@@ -0,0 +1,11 @@
+package com.ticketresolve.ticketresolve;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import com.proyecto.accesoSeguroService.model.Role;
+
+@Repository
+public interface RoleRepository extends CrudRepository<Role, Long>{
+
+}
