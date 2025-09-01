3c6d4553fc84d20cb88e2f622f3d7cde07f62d6b
USUARIO <jeanpoolvalenciasanchez@gmail.com>
2025-08-31 23:09:11 -0500

    actualizacion de rutas

--- src/main/java/com/ticketresolve/ticketresolve/Repository/RoleRepository.java
+++ src/main/java/com/ticketresolve/ticketresolve/Repository/RoleRepository.java
@@ -3,7 +3,7 @@ package com.ticketresolve.ticketresolve.Repository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-import com.proyecto.accesoSeguroService.model.Role;
+import com.ticketresolve.ticketresolve.model.Role;
 
 @Repository
 public interface RoleRepository extends CrudRepository<Role, Long>{
