2dd3206dac7ce2d7ed0b0d4bc6192592fcb5f124
JuanFe <j_felipe05@hotmail.com>
2025-08-31 21:37:01 -0500

    Proyecto inicial Spring Boot

---
+++ src/main/java/com/ticketresolve/ticketresolve/TicketResolveApplication.java
@@ -0,0 +1,13 @@
+package com.ticketresolve.ticketresolve;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketResolveApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TicketResolveApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ticketresolve/ticketresolve/TicketResolveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ticketresolve.ticketresolve;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketResolveApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
