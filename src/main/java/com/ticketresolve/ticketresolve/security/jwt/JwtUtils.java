988420d67fafeff4e964d407063ced5786077a85
Estiven033 <egarciar@estudiante.uniajc.edu.co>
2025-09-01 21:19:40 -0500

    Se agregan las librerias para la clase jwtUtils

--- src/main/java/com/ticketresolve/ticketresolve/security/jwt/JwtUtils.java
+++ src/main/java/com/ticketresolve/ticketresolve/security/jwt/JwtUtils.java
@@ -4,6 +4,13 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+import java.util.function.Function;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+
 import java.security.Key;
 import java.util.Date;
 
